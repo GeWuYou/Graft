c7bc0a8dfdd9ffb0aa689937e89445c8dd7411e9
gewuyou <95328647+GeWuYou@users.noreply.github.com>
2026-05-22 11:15:08 +0800

    fix(web-layout): disable admin footer copyright

--- web/src/layouts/components/PageContainer.vue
+++ web/src/layouts/components/PageContainer.vue
@@ -3,19 +3,14 @@
     <main :class="`${prefix}-page-container__main`">
       <slot />
     </main>
-    <t-footer v-if="showFooter" :class="`${prefix}-footer-layout`">
-      <l-footer :content="footerText" />
-    </t-footer>
     <div :class="`${prefix}-page-container__bottom-safe-spacer`" aria-hidden="true"></div>
   </section>
 </template>
 <script setup lang="ts">
 import { prefix } from '@/config/global';
 
-import LFooter from './Footer.vue';
-
 defineProps<{
-  showFooter: boolean;
-  footerText: string;
+  showFooter?: boolean;
+  footerText?: string;
 }>();
 </script>
