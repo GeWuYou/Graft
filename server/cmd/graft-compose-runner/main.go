e2c87323cf667d7f0fdd347dad95043b818d63f8
Gewuyou <95328647+GeWuYou@users.noreply.github.com>
2026-08-31 00:33:37 +0800

    Revert "fix(compose): align runner docker client"
    This reverts commit e5ba2e657f3464eaa2cf33726a402cf80659c3db.

--- server/cmd/graft-compose-runner/main.go
+++ server/cmd/graft-compose-runner/main.go
@@ -25,7 +25,7 @@ import (
 	dockerflags "github.com/docker/cli/cli/flags"
 	composeapi "github.com/docker/compose/v2/pkg/api"
 	compose "github.com/docker/compose/v2/pkg/compose"
-	dockerclient "github.com/moby/moby/client"
+	dockerclient "github.com/docker/docker/client"
 
 	"graft/server/internal/moduleapi"
 	"graft/server/modules/update"
